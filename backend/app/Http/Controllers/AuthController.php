3c9796f5b207f68606233c109db655b1df4920b1
Philip Cristobal <philipcristobal06@gmail.com>
2024-02-23 11:16:57 +0800

    Created simple api and router. Tested in postman

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -7,6 +7,23 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
     public function verify(Request $request) {
+        $users = User::where('email', $request->email)->get();
 
+        if ($users->isEmpty()) {
+            return response()->json([
+                'message' => 'No user found with the provided email.',
+            ], 404);
+        }
+
+        foreach($users as $user) {
+            $name = $user->user_mname;
+            $activeTag = $user->active_tag;
+        }
+        // $userNames = $users->pluck('user_fname');
+        if($activeTag == 'Y' ? $message = 'User already verified' : $message = '');
+        return response()->json([
+            'message' => 'Users found with the provided email.',
+            'user_names' => $message,
+        ], 200);
     }
 }
