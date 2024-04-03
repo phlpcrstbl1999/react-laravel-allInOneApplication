bd57a73174f6daca30006151e157d5c7082cd659
Philip Cristobal <philipcristobal06@gmail.com>
2024-04-03 10:04:16 +0800

    Added authguard on dashboard route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,13 @@ import 'animate.css';
 import UserRoutes from './components/router/UserRoutes';
 import AuthenticationRouter from './components/router/AuthenticationRoutes';
 import Dashboard from './components/user/Dashboard';
+import UserAuthGuard from './components/router/UserAuthGuard';
 function App() {
   return (
     <div className="App">
      <Router>
         <Routes>
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard" element={<UserAuthGuard><Dashboard /></UserAuthGuard>} />
           <Route path="/user/*" element={<UserRoutes />} />
           <Route path="/authentication/*" element={<AuthenticationRouter />} />
         </Routes>
